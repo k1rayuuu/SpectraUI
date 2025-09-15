65862c79c519344ccf1ae4c675a5a91d824f2e94
ᴋɪʀᴀʏᴜ <shkuratov.r39@gmail.com>
2025-09-15 23:22:58 +0200

    init

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Unity;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using SpectraUI.ViewModels;
+using SpectraUI.Views;
+
+namespace SpectraUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MainView>();
+            containerRegistry.RegisterForNavigation<WallpaperView>();
+            containerRegistry.RegisterForNavigation<TaskBarView>();
+            containerRegistry.RegisterForNavigation<SoundsView>();
+            containerRegistry.RegisterForNavigation<ShareView>();
+            containerRegistry.RegisterForNavigation<UserPageView>();
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.Register<MainView, MainViewModel>();
+            ViewModelLocationProvider.Register<WallpaperView, WallpaperViewModel>();
+            ViewModelLocationProvider.Register<TaskBarView, TaskBarViewModel>();
+            ViewModelLocationProvider.Register<SoundsView, SoundsViewModel>();
+            ViewModelLocationProvider.Register<ShareView, SharePageViewModel>();
+            ViewModelLocationProvider.Register<UserPageView, UserPageViewModel>();
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using SpectraUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpectraUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(RegionManager regionManager)
+        {
+            InitializeComponent();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(MainView));
+        }
+
+        private void HeaderMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
