d788e200291bf034afbf7186903965f2ae726d19
k1rayuuu <shkuratov.r39@gmail.com>
2025-11-13 23:04:43 +0200

    resolve error

---
+++ client/ViewModels/level0/MainPageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using SpectraUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectraUI.ViewModels.level0
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private IRegionManager _regionManager;
+        public DelegateCommand NavigateHomeCommand { get; private set; }
+        public DelegateCommand NavigateWallpaperCommand { get; private set; }
+        public DelegateCommand NavigateTaskBarCommand { get; private set; }
+        public DelegateCommand NavigateSoundsCommand { get; private set; }
+        public DelegateCommand NavigateSharePageCommand { get; private set; }
+        public DelegateCommand NavigateUserPageCommand { get; private set; }
+
+        public MainPageViewModel(IRegionManager regionManager)
+        { 
+            _regionManager = regionManager;
+            NavigateHomeCommand = new DelegateCommand(NavigateHome);
+            NavigateWallpaperCommand = new DelegateCommand(NavigateWallpaper);
+            NavigateTaskBarCommand = new DelegateCommand(NavigateTaskBar);
+            NavigateSoundsCommand = new DelegateCommand(NavigateSounds);
+            NavigateSharePageCommand = new DelegateCommand(NavigateSharePage);
+            NavigateUserPageCommand = new DelegateCommand(NavigateUserPage);
+        }
+
+        private void NavigateHome()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(MainView));
+        }
+
+        private void NavigateWallpaper()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(WallpaperView));
+        }
+
+        private void NavigateTaskBar()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(TaskBarView));
+        }
+
+        private void NavigateSounds()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(SoundsView));
+        }
+
+        private void NavigateSharePage()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(ShareView));
+        }
+
+        private void NavigateUserPage()
+        {
+            _regionManager.RequestNavigate("InnerContentRegion", nameof(UserPageView));
+        }
+    }
+}
---
+++ client/ViewModels/level0/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SpectraUI.Views.level0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectraUI.ViewModels.level0
+{
+    class MainWindowViewModel
+    {
+        private IRegionManager _regionManager;
+        public DelegateCommand NavigateSettingsPageCommand { get; private set; }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            NavigateSettingsPageCommand = new DelegateCommand(NavigateSettingsPage);
+        }
+
+        private void NavigateSettingsPage()
+        {
+            _regionManager.RequestNavigate("ContentRegion", nameof(SettingsPageView));
+        }
+    }
+}
